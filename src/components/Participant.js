96f1e78dcec33f972ac55cdd6aefb4c5be679b76
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-03-07 10:31:42 -0500

    Clean up code

--- src/components/Participant.js
+++ src/components/Participant.js
@@ -4,7 +4,7 @@ import SpeedControl from './SpeedControl';
 
 const formatParticipantName = label => label.replace('spk_', 'speaker_');
 
-const Participant = ({ isSpeaking, name, speed, setSingleSpeakerSpeed }) => (
+const Participant = ({ isSpeaking, name, setSingleSpeakerSpeed, speed }) => (
   <li className={isSpeaking ? 'speaking' : 'silent'}>
     <span className="voiceIndicator" role="img" aria-label="speaker">🔊</span>
     <span className="speakerName">{formatParticipantName(name)}</span>
@@ -19,7 +19,8 @@ const Participant = ({ isSpeaking, name, speed, setSingleSpeakerSpeed }) => (
 Participant.propTypes = {
   isSpeaking: PropTypes.bool.isRequired,
   name: PropTypes.string.isRequired,
-  setSingleSpeakerSpeed: PropTypes.func.isRequired
+  setSingleSpeakerSpeed: PropTypes.func.isRequired,
+  speed: PropTypes.number.isRequired
 };
 
 export default Participant;
--- src/utils/general.js
+++
@@ -1,7 +0,0 @@
-export const checkForEquality = (obj1, obj2) => (
-  Object.keys(obj1).reduce((isSameSoFar, key) => {
-    if (!obj2[key]) return false;
-    if (!(obj1[key] === obj2[key])) return false;
-    return isSameSoFar && true;
-  }, true)
-);
