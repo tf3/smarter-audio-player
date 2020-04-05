b391686eb2817ea11a13dfd39f71d8f41cc5cdb8
Thomas Foerster <39387161+tf3@users.noreply.github.com>
2020-04-05 09:57:33 -0400

    Sort speakers

--- src/utils/getParticipantsFromTimeIntervals.js
+++ src/utils/getParticipantsFromTimeIntervals.js
@@ -1,7 +1,7 @@
 const getParticipantsFromTimeIntervals = timeIntervals => (
   timeIntervals.reduce((participants, { speaker_label }) => (
     participants.includes(speaker_label) ? participants : [...participants, speaker_label]
-  ), [])
+  ), []).sort()
 );
 
 export default getParticipantsFromTimeIntervals;
